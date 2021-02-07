394818beb15e5548970a4428ac4c4cebbcb7c0e4
hp <hp@DESKTOP-0CLOHHV>
2021-02-07 13:59:55 +0530

    Monday Project

---
+++ MyOwnRepository/src/selenium/basic/Learning/MondayProblem.java
@@ -0,0 +1,26 @@
+package selenium.basic.Learning;
+
+public class MondayProblem {
+
+	public static void main(String[] args) {
+		char pass='P',fail='F';
+		
+		char result='P';
+		if(result == pass)
+		{
+			System.out.println("The student is passed");
+		}
+		else if(result== fail)
+		{
+			System.out.println("The student is failed");
+		}
+		
+		else
+		{
+			System.out.println("Invalid input in result");
+		}
+		
+
+	}
+
+}
